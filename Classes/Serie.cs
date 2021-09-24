22418a63204442c6cd249268faa6802079c3040f
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-09-24 20:20:00 -0300

    Projeto Finalizado

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -14,7 +14,7 @@ namespace APP.Series
               this.Ano = ano;
                
         }
-                private Genero Genero {get; set;}
+        private Genero Genero {get; set;}
         private string Titulo {get; set;}
         private string Descricao {get; set;}
 
@@ -36,10 +36,11 @@ namespace APP.Series
         public override string ToString()
         {
             string retorno = "";
-            retorno += "Gênero" + this.Genero + Environment.NewLine;
-            retorno += "Título" + this.Titulo + Environment.NewLine;
-            retorno += "Descrição" + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Início" + this.Ano + Environment.NewLine;
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
             return retorno;
         }
 
@@ -48,9 +49,13 @@ namespace APP.Series
             return this.Titulo;
         }
 
-        public int retonraId()
+        public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
         }
 
         public void Excluir()
--- Program.cs
+++ Program.cs
@@ -19,13 +19,13 @@ namespace APP.Series
                         InserirSerie();
                         break;
                     case "3":
-                        //AtualizarSerie();
+                        AtualizarSerie();
                         break;
                     case "4":
-                        //ExcluirSerie();
+                        ExcluirSerie();
                         break;
                     case "5":
-                        //VisualizarSerie();
+                        VisualizarSerie();
                         break;
                     case "C":
                         Console.Clear();
@@ -43,6 +43,54 @@ namespace APP.Series
            
         }
 
+        private static void ExcluirSerie()
+        {
+            Console.Write("Digite o id da Série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void VisualizarSerie()
+        {
+            Console.Write("Digite o id da Série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+
+        private static void AtualizarSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                            genero: (Genero)entradaGenero,
+                                            titulo: entradaTitulo,
+                                            ano: entradaAno,
+                                            descricao: entradaDescricao);
+
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
         private static void ListarSeries()
                 {
                     Console.WriteLine("Listar Séries");
@@ -55,7 +103,9 @@ namespace APP.Series
                     }
                     foreach (var serie in lista)
                     {
-                        Console.WriteLine("#ID {0}: - {1}", serie.retonraId(), serie.retornaTitulo());
+                        var excluido = serie.retornaExcluido();
+                        
+                        Console.WriteLine("#ID {0}: - {1}  {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "Excluido" : ""));
                     }
                 }
 
